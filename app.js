9e3878a24e766bee5bcaf157546b6a8e07170053
sdonich <sdonich@gmail.com>
2018-12-03 13:47:19 +0200

    v 1.0.0

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const expressHbs = require('express-handlebars');
+const hbs = require('hbs');
+const app = express();
+
+const contact = require('./data/contact');
+  
+// устанавливаем настройки для файлов layout
+app.engine('hbs', expressHbs(
+  {
+    layoutsDir: 'views/layouts', 
+    defaultLayout: 'layout',
+    extname: 'hbs'
+  }
+))
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+ 
+app.use('/contact', function(req, res){
+
+  res.render('contact', contact);
+}); 
+ 
+app.use('/', function(req, res){
+      
+  res.render('home.hbs');
+});
+app.listen(3000, () => console.log('Server is running'));
